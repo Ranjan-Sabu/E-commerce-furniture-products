610a69fa2519d0e5eb66b39a9ae35da8a348e395
Ranjan-Sabu <ranjansabu6@gmail.com>
2023-10-03 11:28:32 +0530

    second commit

--- src/components/Products/Cart.js
+++ src/components/Products/Cart.js
@@ -33,41 +33,63 @@ export default function Cart() {
     });
     setCart(updateCount);
   };
-  
-    
-      return (
-        <section style={{ backgroundColor: "#f9c9aa" }}>
-          <div className="container py-5">
-            <div className="row justify-content-center">
-              <div className="col-md-9 col-lg-7 col-xl-5">
-                {cart.map((item) => (
-                  <div className="row mb-5" key={item.id}>
-                    <div className="col-12">
-                      <div className="card">
-                        <img
-                          src={item.image}
-                          className="card-img-top"
-                          alt={item.name}
-                        />
-                        <div className="card-body">
-                          <div className="card-title d-flex justify-content-between mb-0">
-                            <p className="text-muted mb-0">{item.name}</p>
-                            <button onClick={() => increaseCount(item.id)}>+</button>
-                            <h5>{item.quantity}</h5>
-                            <button onClick={() => decreaseCount(item.id)}>-</button>
-                            <p className="mb-0">₹{item.price * item.quantity}</p>
-                          </div>
-                        </div>
-                        <button onClick={() => removeItem(item.id)}>Delete</button>
-                      </div>
-                    </div>
-                  </div>
-                ))}
-                <div className="rounded-bottom" style={{ backgroundColor: "#eee" }}>
+    return(
+      <section class="h-100" style={{backgroundColor:" #eee;"}}>
+  <div class="container h-100 py-5">
+    <div class="row d-flex justify-content-center align-items-center h-100">
+      <div class="col-10">
+        <div class="d-flex justify-content-between align-items-center mb-4">
+          <h3 class="fw-normal mb-0 text-black">Shopping Cart</h3>
+        </div>
+
+        <div class="card rounded-3 mb-4">
+          <div class="card-body p-4">
+        {cart.map((item) => (
+            <div class="row d-flex justify-content-between align-items-center" key={item.id}>
+              
+              <div class="col-md-2 col-lg-2 col-xl-2">
+                <img
+                  src={item.image}
+                  class="img-fluid rounded-3" alt={item.name}/>
+              </div>
+              <div class="col-md-3 col-lg-3 col-xl-3">
+                <p class="lead fw-normal mb-2">{item.category}</p>
+                <p>{item.name}</p>
+              </div>
+              <div class="col-md-3 col-lg-3 col-xl-2 d-flex align-items-center">
+  <div class="btn-group">
+    <button
+      class="btn btn-outline-primary"
+      onClick={() => increaseCount(item.id)}
+    >
+      <i class="fas fa-plus">+</i>
+    </button>
+    <h5 class="mx-2">{item.quantity}</h5>
+    <button
+      class="btn btn-outline-primary"
+      onClick={() => decreaseCount(item.id)}
+    >
+      <i class="fas fa-minus">-</i>
+    </button>
+  </div>
+</div>
+              <div class="col-md-3 col-lg-2 col-xl-2 offset-lg-1">
+                <h5 class="mb-0">₹{item.price * item.quantity}</h5>
+              </div>
+              <div class="col-md-1 col-lg-1 col-xl-1 text-end">
+              <img src="https://thumbs.dreamstime.com/z/rounded-151782555.jpg?w=768" onClick={() => removeItem(item.id)} style={{width:"50px",height:"50px"}}/>
+              </div>
+            </div>
+                 ))}
+          </div>
+   
+        </div>
+        
+      
+        <div className="rounded-bottom" style={{ backgroundColor: "#eee" }}>
                   <div className="card-body">
-                    <p className="mb-4">Your payment details</p>
-    
-                    <div className="form-outline mb-3">
+                  <p className="mb-4">Your payment details</p>
+                  <div className="form-outline mb-3">
                       <input
                         type="text"
                         id="formControlLgXM8"
@@ -113,15 +135,30 @@ export default function Cart() {
                         </div>
                       </div>
                     </div>
-    
-                    <button className="btn btn-info btn-block">Order now</button>
                   </div>
-                </div>
-              </div>
+                  </div>
+        <div class="card mb-4">
+          <div class="card-body p-4 d-flex flex-row">
+            <div class="form-outline flex-fill">
+              <input type="text" id="form1" class="form-control form-control-lg" />
+              <label class="form-label" for="form1">Discount code</label>
             </div>
+            <button type="button" class="btn btn-outline-warning btn-lg ms-3">Apply</button>
           </div>
-        </section>
-      );
+        </div>
+
+        <div class="card">
+          <div class="card-body">
+            <button type="button" class="btn btn-warning btn-block btn-lg">Proceed to Pay</button>
+          </div>
+        </div>
+
+      </div>
+    </div>
+  </div>
+</section>
+
+        
+   ) }
     
     
-}
